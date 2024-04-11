f97027905ffa0c89fe4bd30549762252c62a6e4a
Murli913 <murlitalreja913@gmail.com>
2024-04-11 13:35:28 +0530

    design the home page for the hospital page

--- Hospital/hospital/src/App.jsx
+++ Hospital/hospital/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   return (
   <>
   <Navbar/>
+  
+  <main>
+        <div id="home">
+          <Home />
+        </div>
+        
+  </main>
   </>
   )
 }
