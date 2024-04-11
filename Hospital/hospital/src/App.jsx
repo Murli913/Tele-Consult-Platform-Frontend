743c87a18ed77a91569a2287f16ff3751b36d17f
Murli913 <murlitalreja913@gmail.com>
2024-04-11 13:43:26 +0530

    design an about page in hospital page

--- Hospital/hospital/src/App.jsx
+++ Hospital/hospital/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
 
 const App = () => {
   return (
@@ -11,7 +12,10 @@ const App = () => {
         <div id="home">
           <Home />
         </div>
-        
+        <div id="about">
+          <About />
+        </div>
+
   </main>
   </>
   )
