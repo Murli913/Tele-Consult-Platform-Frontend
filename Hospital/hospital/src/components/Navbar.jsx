b643c6d684b9e0ee868cb9f6a99b212ff842167b
Murli913 <murlitalreja913@gmail.com>
2024-04-12 05:49:43 +0530

    updated the login page

--- Hospital/hospital/src/components/Navbar.jsx
+++ Hospital/hospital/src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
               className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out"
               onClick={openForm}
             >
-              Contact Us
+             Login
             </button>
           </div>
 
@@ -165,7 +165,7 @@ const Navbar = () => {
               className="bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out"
               onClick={openForm}
             >
-              Contact Us
+              Login
             </button>
           </div>
         </div>
