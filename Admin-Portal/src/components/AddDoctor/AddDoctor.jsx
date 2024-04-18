32699f21c028308e2a50120ef4dccdc988a313d5
Murli913 <murlitalreja913@gmail.com>
2024-04-18 17:41:37 +0530

    Integrated the add doctor page with backend

--- Admin-Portal/src/components/AddDoctor/AddDoctor.jsx
+++ Admin-Portal/src/components/AddDoctor/AddDoctor.jsx
@@ -10,18 +10,38 @@ const AddDoctor = ({ onClose }) => {
   const [gender, setGender] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
-  const navigate = useNavigate();
+  const [isSeniorDoctor, setIsSeniorDoctor] = useState(false);
+  const [seniorDoctorId, setseniorDoctorId] = useState('');
+
+  const handleSeniorDoctorToggle = () => {
+      setIsSeniorDoctor(!isSeniorDoctor);
+      if (!isSeniorDoctor) {
+          setseniorDoctorId(''); // Reset the senior doctor name when toggling off
+      }
+  };
 
   const handleAddDoctor = async () => {
-    try { const response = await axios.post('http://localhost:8080/doctor/register',
-     { name, email, password, gender, phoneNumber  // Include sdid in the request body 
-    },
-     {
-       headers: { 'Authorization': `Bearer ${localStorage.getItem("token")}` }
-       });
+    if(seniorDoctorId === '')
+    {
+      setseniorDoctorId(null);
+    }
+    try { const response = await fetch('http://localhost:8080/admin/register/doctor',{
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json",
+        'Authorization': `Bearer ${localStorage.getItem("token")}`
+      },
+      body: JSON.stringify({
+        name: name,
+        gender: gender,
+        phoneNumber: phoneNumber,
+        email: email,
+        supervisorDoctor: seniorDoctorId
+      }),
+    });
       if (response.status === 200) {
-        console.log(response.data.message); // Log success message
-        onClose(); // Close modal after successful registration
+        console.log(response.data);
+        onClose();
       }
     } catch (error) {
       console.error('Error registering doctor:', error);
@@ -35,9 +55,32 @@ const AddDoctor = ({ onClose }) => {
         <div className="input-fields">
           <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <input type="text" placeholder="Gender" value={gender} onChange={(e) => setGender(e.target.value)} />
-          <input type="text" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          <input type="tel" className='appearance-none' placeholder="Phone Number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          <div>
+            <label>
+                <input 
+                    id='srDocToggle'
+                    type="checkbox" 
+                    checked={isSeniorDoctor} 
+                    onChange={handleSeniorDoctorToggle} 
+                    className="mr-2"
+                />
+                Sr Doctor
+            </label>
+
+            {isSeniorDoctor && (
+                <div>
+                    <input 
+                        type="number" 
+                        placeholder="Senior Doctor ID" 
+                        value={seniorDoctorId} 
+                        onChange={(e) => setseniorDoctorId(e.target.value)} 
+                        className="mt-2 p-2 border border-gray-300 rounded-md"
+                    />
+                </div>
+            )}
+        </div>
           <button className="add-doctor-btn" onClick={handleAddDoctor}>Add Doctor</button>
           <button className="close-btn" onClick={onClose}>Close</button>
         </div>
