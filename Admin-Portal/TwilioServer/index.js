62ae5d9e5948e7629dd92281d75629bd59bc2c6f
Murli913 <murlitalreja913@gmail.com>
2024-04-30 15:49:37 +0530

    integrating

--- Admin-Portal/TwilioServer/index.js
+++ Admin-Portal/TwilioServer/index.js
@@ -1,39 +1,39 @@
-// //Dependencies: 
-// //yarn add express cors twilio 
+//Dependencies: 
+//yarn add express cors twilio 
 
-// const express = require('express'); 
-// const cors = require('cors');
-// const twilio = require('twilio'); 
+const express = require('express'); 
+const cors = require('cors');
+const twilio = require('twilio'); 
 
-// //twilio requirements -- Texting API 
+//twilio requirements -- Texting API 
 // const accountSid = 'AC9df5732424b6dd695b57fce8faad5456';
 // const authToken = 'f477972279d01c5c2c6ea2c07423b1e8'; 
-// const client = new twilio(accountSid, authToken);
+const client = new twilio(accountSid, authToken);
 
-// const app = express(); //alias
+const app = express(); //alias
 
-// app.use(cors()); //Blocks browser from restricting any data
+app.use(cors()); //Blocks browser from restricting any data
 
-// //Welcome Page for the Server 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the Express Server')
-// })
+//Welcome Page for the Server 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express Server')
+})
 
-// app.get('/send-text', (req, res) => {
-//     //Welcome Message
-//     res.send('Hello to the Twilio Server');
+app.get('/send-text', (req, res) => {
+    //Welcome Message
+    res.send('Hello to the Twilio Server');
 
-//     //_GET Variables
-//     const recipient = req.query.recipient;
-//     const textmessage = req.query.textmessage;
+    //_GET Variables
+    const recipient = req.query.recipient;
+    const textmessage = req.query.textmessage;
 
-//     //Send Text
-//     const recipientPhoneNumber = '+91' + recipient.replace(/\D/g, ''); // Remove non-numeric characters and prepend country code
-//     client.messages.create({
-//         body: textmessage,
-//         to: recipientPhoneNumber,
-//         from: '+14242519807' // Your Twilio phone number
-//     }).then((message) => console.log(message.body));
-// });
+    //Send Text
+    const recipientPhoneNumber = '+91' + recipient.replace(/\D/g, ''); // Remove non-numeric characters and prepend country code
+    client.messages.create({
+        body: textmessage,
+        to: recipientPhoneNumber,
+        from: '+14242519807' // Your Twilio phone number
+    }).then((message) => console.log(message.body));
+});
 
-// app.listen(4000, () => console.log("Running on Port 4000"))
+app.listen(4000, () => console.log("Running on Port 4000"))
--- Admin-Portal/src/components/Message/Message.jsx
+++ Admin-Portal/src/components/Message/Message.jsx
@@ -1,110 +1,110 @@
-// import React, { Component } from 'react';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import TextField from '@material-ui/core/TextField';
-// import Button from '@material-ui/core/Button';
-// import Snackbar from '@material-ui/core/Snackbar';
-// import MuiAlert from '@material-ui/lab/Alert';
-// import './Message.css';
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import './Message.css';
 
-// class Message extends Component {
-//   state = {
-//     text: {
-//       recipient: '',
-//       textmessage: ''
-//     },
-//     notification: {
-//       open: false,
-//       message: '',
-//       severity: 'success'
-//     }
-//   };
+class Message extends Component {
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    },
+    notification: {
+      open: false,
+      message: '',
+      severity: 'success'
+    }
+  };
 
-//   sendText = () => {
-//     const { text } = this.state;
-//     //pass text message GET variables via query string
-//     fetch(`http://127.0.0.1:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
-//       .then(response => {
-//         if (response.ok) {
-//           this.setState({
-//             notification: {
-//               open: true,
-//               message: 'Message sent successfully',
-//               severity: 'success'
-//             }
-//           });
-//         } else {
-//           this.setState({
-//             notification: {
-//               open: true,
-//               message: 'Failed to send message',
-//               severity: 'error'
-//             }
-//           });
-//         }
-//       })
-//       .catch(err => console.error(err));
-//   };
+  sendText = () => {
+    const { text } = this.state;
+    //pass text message GET variables via query string
+    fetch(`http://127.0.0.1:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
+      .then(response => {
+        if (response.ok) {
+          this.setState({
+            notification: {
+              open: true,
+              message: 'Message sent successfully',
+              severity: 'success'
+            }
+          });
+        } else {
+          this.setState({
+            notification: {
+              open: true,
+              message: 'Failed to send message',
+              severity: 'error'
+            }
+          });
+        }
+      })
+      .catch(err => console.error(err));
+  };
 
-//   handleNotificationClose = () => {
-//     this.setState({
-//       notification: {
-//         ...this.state.notification,
-//         open: false
-//       }
-//     });
-//   };
+  handleNotificationClose = () => {
+    this.setState({
+      notification: {
+        ...this.state.notification,
+        open: false
+      }
+    });
+  };
 
-//   render() {
-//     const { text, notification } = this.state;
-//     return (
-//       <div className="App">
+  render() {
+    const { text, notification } = this.state;
+    return (
+      <div className="App">
  
-//         <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-//           <Card>
-//             <CardContent>
-//               <h2>Phone Number Validation</h2>
-//               <div style={{ marginBottom: '20px' }}>
-//                 <TextField
-//                   label="Your Phone Number"
-//                   variant="outlined"
-//                   value={text.recipient}
-//                   onChange={e => this.setState({ text: { ...text, recipient: e.target.value } })}
-//                   fullWidth
-//                 />
-//               </div>
-//               <div style={{ marginBottom: '20px' }}>
-//                 <TextField
-//                   label="Enter the code"
-//                   variant="outlined"
-//                   multiline
-//                   rows={3}
-//                   value={text.textmessage}
-//                   onChange={e => this.setState({ text: { ...text, textmessage: e.target.value } })}
-//                   fullWidth
-//                 />
-//               </div>
-//               <div>
-//                 <Button   style={{ width: '250px' }} variant="contained" color="primary" onClick={this.sendText}>
-//                   Send OTP
-//                 </Button>
-//               </div>
-//             </CardContent>
-//           </Card>
-//         </div>
-//         <Snackbar open={notification.open} autoHideDuration={6000} onClose={this.handleNotificationClose}>
-//           <MuiAlert
-//             elevation={6}
-//             variant="filled"
-//             onClose={this.handleNotificationClose}
-//             severity={notification.severity}
-//           >
-//             {notification.message}
-//           </MuiAlert>
-//         </Snackbar>
-//       </div>
-//     );
-//   }
-// }
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <Card>
+            <CardContent>
+              <h2>Phone Number Validation</h2>
+              <div style={{ marginBottom: '20px' }}>
+                <TextField
+                  label="Your Phone Number"
+                  variant="outlined"
+                  value={text.recipient}
+                  onChange={e => this.setState({ text: { ...text, recipient: e.target.value } })}
+                  fullWidth
+                />
+              </div>
+              <div style={{ marginBottom: '20px' }}>
+                <TextField
+                  label="Enter the code"
+                  variant="outlined"
+                  multiline
+                  rows={3}
+                  value={text.textmessage}
+                  onChange={e => this.setState({ text: { ...text, textmessage: e.target.value } })}
+                  fullWidth
+                />
+              </div>
+              <div>
+                <Button   style={{ width: '250px' }} variant="contained" color="primary" onClick={this.sendText}>
+                  Send OTP
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Snackbar open={notification.open} autoHideDuration={6000} onClose={this.handleNotificationClose}>
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            onClose={this.handleNotificationClose}
+            severity={notification.severity}
+          >
+            {notification.message}
+          </MuiAlert>
+        </Snackbar>
+      </div>
+    );
+  }
+}
 
-// export default Message;
+export default Message;
